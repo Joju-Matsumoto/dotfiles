4de6da365da4cd1a6dcfb5233b96b89704961826
jj_ <joju.matsumoto@satori.team>
2022-06-10 04:27:20 +0000

    move dot files

--- install.sh
+++ install.sh
@@ -2,18 +2,20 @@
 
 DOTPATH=~/.dotfiles
 
-if type "git"; then
-  git clone --recursive "https://github.com/Joju-Matsumoto/dotfiles.git" "$DOTPATH"
-elif type "curl" || type "wget"; then
-  tarball="https://codeload.github.com/Joju-Matsumoto/dotfiles/archive/main.tar.gz"
+if [ ! -d $DOTPATH ]; then
+  if type "git"; then
+    git clone --recursive "https://github.com/Joju-Matsumoto/dotfiles.git" "$DOTPATH"
+  elif type "curl" || type "wget"; then
+    tarball="https://codeload.github.com/Joju-Matsumoto/dotfiles/archive/main.tar.gz"
 
-  if type "curl"; then
-    curl -L "$tarball"
-  elif type "wget"; then
-    wget -O - "$tarball"
-  fi | tar zxv
-else
-  die "curl or wget required."
+    if type "curl"; then
+      curl -L "$tarball"
+    elif type "wget"; then
+      wget -O - "$tarball"
+    fi | tar zxv
+  else
+    die "curl or wget required."
+  fi
 fi
 
 cd $DOTPATH
@@ -21,7 +23,7 @@ if [ $? -ne 0 ]; then
   die "not found: $DOTPATH"
   exit 1
 fi
-
+  
 for f in .??*
 do
   [ "$f" = ".git" ] && continue
@@ -29,6 +31,6 @@ do
   if [ -e "$HOME/$f" ]; then
     mv "$HOME/$f" "$HOME/$f.backup" && echo "backup: $f"
   fi
-
+  
   ln -snvf "$DOTPATH/$f" "$HOME/$f" && echo "link: $f"
 done
